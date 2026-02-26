850a2c9a02a90933efe30c9ba08e2a7cdf361d2f
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-02-26 13:48:11 +0530

    refactor(user): update user model fields

--- app/backend/internal/user/model.go
+++ app/backend/internal/user/model.go
@@ -1,11 +1,10 @@
 package user
 
 type User struct {
-	ID         int
-	Email      string
-	Name       string
-	Provider   string
-	ProviderID string
+	ID           string
+	Email        string
+	Name         string
+	Provider     string
+	ProviderID   string
 	RefreshToken string
 }
-
