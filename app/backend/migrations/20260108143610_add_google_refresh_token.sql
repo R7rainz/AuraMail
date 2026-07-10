97652484df47fa431879f9175037b3238b26c6d4
rainz <ronakkamboj26@gmail.com>
2026-07-10 16:41:27 +0530

    cleanup

--- app/backend/migrations/20260108143610_add_google_refresh_token.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE users ADD COLUMN google_refresh_token TEXT;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE users DROP COLUMN google_refresh_token;
--- +goose StatementEnd
--- app/frontend/src/app/dashboard/ResponsiveGrid.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-import { ResponsiveGridLayout } from "react-grid-layout";
-
-export default ResponsiveGridLayout;
